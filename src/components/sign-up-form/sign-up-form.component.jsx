499ad8ef5720d907a60ac07e06abf4ff35fe154b
Xiaoxi Yang <yang.xiaoxi1@northeastern.edu>
2023-04-28 11:25:02 -0700

    update authentication page

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
 
 import { 
   createAuthUserWithEmailAndPassword, 
@@ -91,7 +92,7 @@ const SignUpForm = () => {
           name="confirmPassword" 
           value={confirmPassword}
         />
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   );
