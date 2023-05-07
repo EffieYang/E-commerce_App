c18657007702fc0b4af66aa430a184cb011d6fc0
Xiaoxi Yang <yang.xiaoxi1@northeastern.edu>
2023-05-06 23:00:23 -0700

    update existing redux files to redux-toolkit

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,7 +1,6 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
-import { selectCartItems } from "../../store/cart/cart.selector";
-import { addItemToCart } from "../../store/cart/cart.action";
+import { addItemToCart } from '../../store/cart/cart.reducer';
 
 import Button, {BUTTON_TYPE_CLASSES} from "../button/button.component";
 
@@ -15,9 +14,8 @@ import {
 const ProductCard = ({product}) => {
   const {name, price, imageUrl} = product;
   const dispatch = useDispatch();
-  const cartItems = useSelector(selectCartItems);
 
-  const addProductToCart = () => dispatch(addItemToCart(cartItems, product));
+  const addProductToCart = () => dispatch(addItemToCart(product));
 
   return (
     <ProductCartContainer>
@@ -26,14 +24,14 @@ const ProductCard = ({product}) => {
         <Name>{name}</Name>
         <Price>{price}</Price>
       </Footer>
-      <Button 
-        buttonType={BUTTON_TYPE_CLASSES.inverted} 
+      <Button
+        buttonType={BUTTON_TYPE_CLASSES.inverted}
         onClick={addProductToCart}
       >
         Add to card
       </Button>
     </ProductCartContainer>
-  );  
+  );
 };
 
 export default ProductCard;
--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,28 +1,25 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useState } from 'react';
 
-import FormInput from "../form-input/form-input.component";
-import Button from "../button/button.component";
+import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
 
-import { 
-  createAuthUserWithEmailAndPassword, 
+import {
+  createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
-} from "../../utils/firebase/firebase.utils";
+} from '../../utils/firebase/firebase.utils';
 
-import { SignUpContainer } from "./sign-up-form.styles";
-import { signUpStart } from "../../store/user/user.action";
+import { SignUpContainer } from './sign-up-form.styles';
 
 const defaultFormFields = {
-  displayName: "",
-  email: "",
-  password: "",
-  confirmPassword: "",
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
 };
 
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
-  const {displayName, email, password, confirmPassword} = formFields;
-  const dispatch = useDispatch();
+  const { displayName, email, password, confirmPassword } = formFields;
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
@@ -31,25 +28,32 @@ const SignUpForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (password !== confirmPassword){
-      alert("passwords do not match");
+    if (password !== confirmPassword) {
+      alert('passwords do not match');
       return;
     }
+
     try {
-      dispatch(signUpStart(email, password, displayName));
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      await createUserDocumentFromAuth(user, { displayName });
       resetFormFields();
-    } catch(error) {
-      if (error.code === "auth/email-already-in-use") {
-        alert("Cannot create user, email already in use");
+    } catch (error) {
+      if (error.code === 'auth/email-already-in-use') {
+        alert('Cannot create user, email already in use');
       } else {
-        console.log("user creation encountered an error", error);
+        console.log('user creation encountered an error', error);
       }
     }
   };
 
   const handleChange = (event) => {
-    const {name, value} = event.target;
-    setFormFields({...formFields, [name]: value});
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
   };
 
   return (
@@ -58,41 +62,41 @@ const SignUpForm = () => {
       <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
-          label="Display Name" 
-          type="text" 
-          required 
-          onChange={handleChange} 
-          name="displayName" 
-          value={displayName} 
+          label='Display Name'
+          type='text'
+          required
+          onChange={handleChange}
+          name='displayName'
+          value={displayName}
         />
 
         <FormInput
-          label="Email" 
-          type="email" 
-          required 
-          onChange={handleChange} 
-          name="email" 
-          value={email} 
+          label='Email'
+          type='email'
+          required
+          onChange={handleChange}
+          name='email'
+          value={email}
         />
 
         <FormInput
-          label="Password"
-          type="password" 
-          required 
-          onChange={handleChange} 
-          name="password" 
+          label='Password'
+          type='password'
+          required
+          onChange={handleChange}
+          name='password'
           value={password}
         />
 
-        <FormInput 
-          label="Confirm password"
-          type="password" 
-          required 
-          onChange={handleChange} 
-          name="confirmPassword" 
+        <FormInput
+          label='Confirm Password'
+          type='password'
+          required
+          onChange={handleChange}
+          name='confirmPassword'
           value={confirmPassword}
         />
-        <Button type="submit">Sign Up</Button>
+        <Button type='submit'>Sign Up</Button>
       </form>
     </SignUpContainer>
   );
